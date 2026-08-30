57f9962c2b8ba6f04e86204cbe6860178614b635
Kunal7006 <91647307+Kunal7006@users.noreply.github.com>
2026-08-30 16:53:03 +0530

    Time: 527 ms (82.27%), Space: 22.4 MB (31.86%) - LeetHub

---
+++ 0015-3sum/0015-3sum.py
@@ -0,0 +1,50 @@
+class Solution:
+    def threeSum(self, nums: list[int]) -> list[list[int]]:
+        nums.sort()
+        ans = []
+
+        n = len(nums)
+
+        for i in range(n - 2):
+
+            # Skip duplicate values for i
+            if i > 0 and nums[i] == nums[i - 1]:
+                continue
+
+            # If nums[i] > 0, all following numbers
+            # are also positive
+            if nums[i] > 0:
+                break
+
+            left = i + 1
+            right = n - 1
+
+            while left < right:
+
+                total = nums[i] + nums[left] + nums[right]
+
+                if total == 0:
+                    ans.append([
+                        nums[i],
+                        nums[left],
+                        nums[right]
+                    ])
+
+                    left += 1
+                    right -= 1
+
+                    # Skip duplicate left values
+                    while left < right and nums[left] == nums[left - 1]:
+                        left += 1
+
+                    # Skip duplicate right values
+                    while left < right and nums[right] == nums[right + 1]:
+                        right -= 1
+
+                elif total < 0:
+                    left += 1
+
+                else:
+                    right -= 1
+
+        return ans
