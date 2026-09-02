c4f97782675dc7d3e2cdc0ec2b66193b500c726e
Kunal7006 <91647307+Kunal7006@users.noreply.github.com>
2026-09-02 13:52:01 +0530

    Time: 0 ms (100%), Space: 19.3 MB (62.64%) - LeetHub

--- 0202-happy-number/0202-happy-number.py
+++ 0202-happy-number/0202-happy-number.py
@@ -1,5 +1,5 @@
 class Solution:
-    def genNext(self, n: int) -> int:
+    def getNext(self, n):
         total = 0
 
         while n > 0:
@@ -9,16 +9,16 @@ class Solution:
 
         return total
 
-    def isHappy(self, n: int) -> bool:
-        fast = n
-        slow = n
+    def isHappy(self, n):
+        seen = set()
 
-        while True:
-            slow = self.genNext(slow)
-            fast = self.genNext(self.genNext(fast))
-
-            if slow == 1:
-                return True
-
-            if slow == fast:
+        while n != 1:
+            # If n is already seen, we are in a cycle
+            if n in seen:
                 return False
+
+            seen.add(n)
+
+            n = self.getNext(n)
+
+        return True
