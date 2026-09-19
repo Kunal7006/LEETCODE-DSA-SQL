70fee80a5c6041f7f54af2fc2fbe9999f88610a5
Kunal7006 <91647307+Kunal7006@users.noreply.github.com>
2026-09-19 14:57:19 +0530

    Time: 1481 ms (89.72%), Space: 0B (100%) - LeetHub

---
+++ 1907-count-salary-categories/1907-count-salary-categories.sql
@@ -0,0 +1,14 @@
+select "Low Salary" as category,
+count(account_id)  as accounts_count
+from Accounts
+where income<20000
+UNION ALL
+select "Average Salary" as category,
+count(account_id)  as accounts_count
+from Accounts
+where income>=20000 and income <=50000
+UNION ALL
+select "High Salary" as category,
+count(account_id)  as accounts_count
+from Accounts
+where income>50000
