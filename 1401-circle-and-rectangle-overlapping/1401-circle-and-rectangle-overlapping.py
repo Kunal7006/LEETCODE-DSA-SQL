7a0bf39b32850209f46ee081952e98bcc9ee79cc
Kunal7006 <91647307+Kunal7006@users.noreply.github.com>
2026-09-19 13:02:05 +0530

    Time: 0 ms (100%), Space: 19.4 MB (18.67%) - LeetHub

--- 1401-circle-and-rectangle-overlapping/1401-circle-and-rectangle-overlapping.py
+++ 1401-circle-and-rectangle-overlapping/1401-circle-and-rectangle-overlapping.py
@@ -1,23 +1,32 @@
 class Solution:
-    def checkOverlap(self, radius: int, xCenter: int, yCenter: int, x1: int, y1: int, x2: int, y2: int) -> bool:
-        
-        if x1> xCenter:
-            xi=x1
-        elif x2< xCenter:
+    def checkOverlap(
+        self,
+        radius: int,
+        xCenter: int,
+        yCenter: int,
+        x1: int,
+        y1: int,
+        x2: int,
+        y2: int,
+    ) -> bool:
+
+        if x1 > xCenter:
+            xi = x1
+        elif x2 < xCenter:
             xi = x2
         else:
             xi = xCenter
-        
-        if y1> yCenter:
-            yi=y1
-        elif y2< yCenter:
+
+        if y1 > yCenter:
+            yi = y1
+        elif y2 < yCenter:
             yi = y2
         else:
             yi = yCenter
-        
-        d = ((xi - xCenter) ** 2 + (yi-yCenter)**2)**0.5
 
-        if radius>=d:
+        d = ((xi - xCenter) ** 2 + (yi - yCenter) ** 2) ** 0.5
+
+        if radius >= d:
             return True
         else:
             return False
